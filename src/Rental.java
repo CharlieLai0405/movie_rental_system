363649477ab1cd246919b65f2a79b74de4553553
葉皓允 <ryanyeh818@gmail.com>
2025-05-15 13:57:04 +0800

    Add Rental and MovieType

--- src/Rental.java
+++ src/Rental.java
@@ -1,3 +1,22 @@
 public class Rental {
 
+    public double getCharge() {
+        MovieType type = movie.getPriceCode();
+        double charge;
+
+        if (type == MovieType.NEW_RELEASE) {
+            charge = daysRented * type.getBasePrice();
+        } else {
+            charge = type.getBasePrice();
+            if (daysRented > type.getDefaultDays()) {
+                charge += (daysRented - type.getDefaultDays()) * type.getDelayedPrice();
+            }
+        }
+
+    return charge;
+    }
+
+    public double getFrequentRenterPoints() {
+        return movie.getPriceCode().getPoint();
+    }
 }
