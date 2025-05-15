764275ee67d93d26142de0ee40250da487c92ae3
Charlie <86840060+CharlieLai0405@users.noreply.github.com>
2025-05-15 13:57:57 +0800

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -1,3 +1,37 @@
 public class Movie {
+    private String name;
+    private MovieType type;
 
+    public Movie(String name, MovieType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public double getCharge(int daysRented) {
+        double base = type.getBasePrice();
+        int defaultDays = type.getDefaultDays();
+        double delayedPrice = type.getDelayedPrice();
+
+        if (daysRented <= defaultDays) {
+            return base;
+        } else {
+            return base + (daysRented - defaultDays) * delayedPrice;
+        }
+    }
+
+    public double getPoints() {
+        return type.getPoint();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MovieType getType() {
+        return type;
+    }
+
+    public void setType(MovieType newType) {
+        this.type = newType;
+    }
 }
